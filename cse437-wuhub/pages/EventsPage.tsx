b4d18881f3caa09a6810e1ded01e8de8631809ef
Jeremy Stiava <j.j.stiava@wustl.edu>
2023-02-26 15:23:12 -0600

    added async database connection

--- cse437-wuhub/pages/EventsPage.tsx
+++ cse437-wuhub/pages/EventsPage.tsx
@@ -1,29 +1,95 @@
+import React, { useState, useEffect, ReactElement } from 'react';
+import { GetStaticProps, NextPage } from 'next';
 import { initializeApp } from 'firebase/app';
+import { useRouter } from "next/router";
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import { useRouter } from "next/router"
 import { init_firebase } from '@/firebase/firebase-config';
+import { init_firestore } from '../firebase/firebase-config';
+import { useDocument, useCollection } from 'react-firebase-hooks/firestore';
+import { getFirestore, collection, Firestore, getDoc, getDocs, deleteDoc, doc, Timestamp, DocumentReference } from 'firebase/firestore';
 
-// define the Student Dashboard functional component
-export default function ProfilePage() {
+const firebase = init_firebase(); // initialize the Firebase app
+const auth = getAuth(); // get the authentication object
+const firestore = init_firestore();
 
-    // Initialize Firebase
-    const firebase = init_firebase(); // initialize the Firebase app
-    const auth = getAuth(); // get the authentication object
+// Expected database schema
+interface Event {
+    id: string;
+    title: string;
+    start: string;
+    end: string;
+    organization: string;
+}
+
+interface Organization {
+    name: string;
+}
+
+interface Props {
+    posts: Event[];
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+
+    const postsCollection = await collection(firestore, 'events');
+    const postsQuerySnapshot = await getDocs(postsCollection);
+
+    const postData: Event[] = [];
+    postsQuerySnapshot.forEach((doc) => {
+        const data = doc.data();
+
+        postData.push({
+            id: doc.id,
+            title: data.title,
+            start: data.start.toDate().toISOString(),
+            end: data.end.toDate().toISOString(),
+            organization: data.organization,
+        } as Event);
+    });
+
+    return {
+        props: { posts: postData },
+    };
+};
+
+export default function EventsPage({ posts }: Props) {
 
     const router = useRouter();
 
     const backClick = () => {
-        console.log("profile click");
         router.push('/StudentDashboard');
     }
 
-    // render the Student Dashboard page
+    const [deletedPostId, setDeletedPostId] = useState<string | null>(null);
+
+    const handleDeletePost = async (postId: string) => {
+        await deleteDoc(doc(firestore, 'events', postId));
+        setDeletedPostId(postId);
+    };
+
+    if (!posts) {
+        return <div>Loading...</div>;
+    }
+
     return (
-        <div>
+        <>
             <button onClick={backClick} className="btn"> Back to Dashboard </button>
-            <br />
-            A list of events...
-            <br />
-        </div>
+            <h1>Events</h1>
+            {
+                posts.map((post) => {
+                    return (
+                        <div key={post.id}>
+                            {console.log(post)}
+                            <h2>{post.title}</h2>
+                            <p>{post.start}</p>
+                            <p>{post.end}</p>
+                            <p>{post.organization}</p>
+                            <button onClick={() => handleDeletePost(post.id)}>Delete</button>
+                            {deletedPostId === post.id && <p>Post deleted!</p>}
+                        </div>
+                    )
+                })
+            }
+        </>
     );
-}
+};
--- cse437-wuhub/pages/OrganizationsPage.tsx
+++ cse437-wuhub/pages/OrganizationsPage.tsx
@@ -1,30 +1,83 @@
+import React, { useState, useEffect, ReactElement } from 'react';
+import { GetStaticProps, NextPage } from 'next';
 import { initializeApp } from 'firebase/app';
+import { useRouter } from "next/router";
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import { useRouter } from "next/router"
 import { init_firebase } from '@/firebase/firebase-config';
+import { init_firestore } from '../firebase/firebase-config';
+import { useDocument, useCollection } from 'react-firebase-hooks/firestore';
+import { getFirestore, collection, Firestore, getDoc, getDocs, deleteDoc, doc, Timestamp, DocumentReference } from 'firebase/firestore';
 
-// define the Student Dashboard functional component
-export default function ProfilePage() {
 
-    // Initialize Firebase
-    const firebase = init_firebase(); // initialize the Firebase app
-    const auth = getAuth(); // get the authentication object
+const firebase = init_firebase(); // initialize the Firebase app
+const auth = getAuth(); // get the authentication object
+const firestore = init_firestore();
+
+// Expected database schema
+interface Organization {
+    id: string;
+    name: string;
+}
+
+
+interface Props {
+    posts: Organization[];
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+
+    const postsCollection = await collection(firestore, 'organizations');
+    const postsQuerySnapshot = await getDocs(postsCollection);
+
+    const postData: Organization[] = [];
+    postsQuerySnapshot.forEach((doc) => {
+        const data = doc.data();
+
+        postData.push({
+            id: doc.id,
+            name: data.name,
+        } as Organization);
+    });
+
+    return {
+        props: { posts: postData },
+    };
+};
+
+export default function OrganizationsPage({ posts }: Props) {
 
     const router = useRouter();
 
+    const [deletedPostId, setDeletedPostId] = useState<string | null>(null);
+
     const backClick = () => {
-        console.log("profile click");
         router.push('/StudentDashboard');
     }
 
+    const handleDeletePost = async (postId: string) => {
+        await deleteDoc(doc(firestore, 'organizations', postId));
+        setDeletedPostId(postId);
+    };
 
-    // render the Student Dashboard page
+    if (!posts) {
+        return <div>Loading...</div>;
+    }
+
     return (
-        <div>
+        <>
             <button onClick={backClick} className="btn"> Back to Dashboard </button>
-            <br />
-            A list of organizations...
-            <br />
-        </div>
+            <h1>Organizations</h1>
+            {
+                posts.map((post) => {
+                    return (
+                        <div key={post.id}>
+                            <h2>{post.name}</h2>
+                            <button onClick={() => handleDeletePost(post.id)}>Delete</button>
+                            {deletedPostId === post.id && <p>Post deleted!</p>}
+                        </div>
+                    )
+                })
+            }
+        </>
     );
-}
+};
