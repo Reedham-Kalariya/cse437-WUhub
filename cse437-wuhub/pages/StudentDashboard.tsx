c653dbbed97eb921eb913bf9566118296cbdb497
F-Stephanie <f.stephanie@wustl.edu>
2023-03-07 16:48:45 -0600

    fixing issues with branching

--- cse437-wuhub/pages/StudentDashboard.tsx
+++ cse437-wuhub/pages/StudentDashboard.tsx
@@ -11,16 +11,6 @@ export default function StudentDashboard() {
     const firebase = init_firebase(); // initialize the Firebase app
     const auth = getAuth(); // get the authentication object
 
-    //function to get the three closest events and display
-    const displayOrgs = () => {
-        //TODO: access the organizations in firebase, grab 3ish and put them in the span
-    }
-
-    //function to get three organizations and dispaly
-    const displayEvents = () => {
-        //TODO: access the events in firebase, grab 3ish and put them in the span
-    }
-
     const router = useRouter();
 
     const logoutClick = () => {
