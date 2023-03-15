40bd32ef551ac4010c474c8423a395ea402ed5c6
Jeremy Stiava <j.j.stiava@wustl.edu>
2023-03-15 06:55:18 -0500

    API setup and dynamic routing

---
+++ cse437-wuhub/pages/api/organization/[id].ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { init_firebase, init_firebase_storage } from "@/firebase/firebase-config";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const id = req.query.id as string;
+    
+    console.log(id);
+    const firestore = init_firebase_storage();
+
+    // Get document by ID
+    const docRef = doc(firestore, "organizations/" + id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        const data = docSnap.data();
+        res.status(200).json(data);
+    } else {
+        res.status(404).end("error");
+    }
+
+}
